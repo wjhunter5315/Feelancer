b258378dc3aa7082d7f03eb1fb25b789d674d9c6
Glenny <55863951+anthonychry@users.noreply.github.com>
2019-12-16 15:00:16 -0800

    edited config & schema for deployment

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var exphbs = require("express-handlebars");
 
 var app = express();
-var PORT = process.env.PORT || 8000;
+var PORT = process.env.PORT || 3000;
 var db = require("./models");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
